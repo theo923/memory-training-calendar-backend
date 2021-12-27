96b8061bc3babc4c465e8b90f6a44c518dfb8404
theo923 <theotam912@gmail.com>
2021-12-28 07:30:10 +0800

    sync with frontend, added t_finished column for task

---
+++ src/api/user-task/controllers/user-task.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  user-task controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::user-task.user-task');
---
+++ src/api/user-task/routes/user-task.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-task router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::user-task.user-task');
