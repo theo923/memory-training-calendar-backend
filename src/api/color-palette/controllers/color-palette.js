5fa4cc6bddc9f03a13103bd71c0512c9b96b3519
theo923 <theotam912@gmail.com>
2022-01-01 14:22:48 +0800

    updated for server settings

---
+++ src/api/color-palette/controllers/color-palette.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  color-palette controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::color-palette.color-palette');
