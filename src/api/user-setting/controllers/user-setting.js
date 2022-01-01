a68c748f9e3e3469f93dc0e3321bfe90d9f3e0ea
theo923 <theotam912@gmail.com>
2022-01-02 04:25:11 +0800

    added support for customizable background

---
+++ src/api/user-setting/controllers/user-setting.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  user-setting controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::user-setting.user-setting');
---
+++ src/api/user-setting/routes/user-setting.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-setting router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::user-setting.user-setting');
